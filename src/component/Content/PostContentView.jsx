b9379874811cc88c210d0c02ad4644cb710b4bf0
Alfiya Maximova <Alfiya.Maximova@gmail.com>
2022-04-09 14:12:01 +0200

    Post loading moved from post view page (parent) into content view component (child) since post data is used only in child component

--- src/component/Content/PostContentView.jsx
+++ src/component/Content/PostContentView.jsx
@@ -1,5 +1,5 @@
-import React, { useContext, useMemo, useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import React, { useContext, useEffect, useMemo, useState } from 'react';
+import { useNavigate, useParams } from 'react-router-dom';
 
 import css from './Content.module.css'
 
@@ -12,13 +12,35 @@ import { Button, Divider, message, Tag, Timeline } from 'antd';
 import { formatDateTime } from '../../utils/date-utils';
 import NavLine from '../NavLine/NavLine';
 import { AppContext } from '../../context';
-import { deletePost } from '../../service/post-service';
+import { deletePost, getPostById } from '../../service/post-service';
+import Spinner from '../Spinner/Spinner';
 
-function PostContentView({post}) {
+function PostContentView() {
+
+    const {postId} = useParams();
+
+    const [post, setPost] = useState({});
+    const [isPostLoadingInProgress, setIsPostLoadingInProgress] = useState(false);
+    const [isConfirmationDialogVisible, setIsConfirmationDialogVisible] = useState(false);
 
     const {loggedInUser, setPostChanged} = useContext(AppContext);
-    const [isConfirmationDialogVisible, setIsConfirmationDialogVisible] = useState(false);
+
     const navigate = useNavigate();
+
+    useEffect(() => {
+        if (!!postId) {
+            setIsPostLoadingInProgress(true);
+
+            getPostById(postId)
+                .then(post => setPost(post))
+                .catch(error => {
+                    setPost({});
+
+                    message.error(`Не удалось загрузить постинг для просмотра: ${error.message}`);
+                })
+                .finally(() => setIsPostLoadingInProgress(false));
+        }
+    }, [postId]);
 
     const canChangePost = useMemo(() => {
         return !!post && !!post.author && !!loggedInUser && post.author._id === loggedInUser._id;
@@ -54,45 +76,47 @@ function PostContentView({post}) {
     }
 
     return (
-        <main className={css.postContentBlock}>
-            <NavLine currentLocationTitle='Просмотр постинга'/>
-            <div className={css.headerAndButtonsSection}>
-                <h2>{post?.title}</h2>
-                {canChangePost && <div className={css.editAndDeleteButtonsSection}>
-                    <Button onClick={onEditClick}>Редактировать</Button>
-                    <Button onClick={onDeleteClick}>Удалить</Button>
-                </div>}
-            </div>
-            <Meta
-                avatar={<Avatar src={post?.author?.avatar} />}
-                title={post?.author?.email}
-            />
-            <Divider />
-            <Timeline>
-                <Timeline.Item>{formatDateTime(post?.created_at)}</Timeline.Item>
-                <Timeline.Item color="green">Отредактировано: {formatDateTime(post?.updated_at)}</Timeline.Item>
-            </Timeline>
-            <Divider />
-                {post?.text}
-            <Divider />
-            <div>
-                {post?.tags?.map(tag => <Tag key={tag}>{tag}</Tag>)}
-            </div>
-
-            <Modal
-                title="Подтверждение"
-                visible={isConfirmationDialogVisible}
-                onOk={onDeleteConfirmedClick}
-                okText='Да, удалить'
-                onCancel={onDeleteCanceledClick}
-                cancelText='Отмена'
-                centered={true}>
-                <div className={css.questionIconAndText}>
-                    <QuestionCircleOutlined className={css.questionIcon}/>
-                    <span>Вы уверены, что хотите удалить данный постинг?</span>
+        <Spinner spinning={isPostLoadingInProgress}>
+            <main className={css.postContentBlock}>
+                <NavLine currentLocationTitle='Просмотр постинга'/>
+                <div className={css.headerAndButtonsSection}>
+                    <h2>{post?.title}</h2>
+                    {canChangePost && <div className={css.editAndDeleteButtonsSection}>
+                        <Button onClick={onEditClick}>Редактировать</Button>
+                        <Button onClick={onDeleteClick}>Удалить</Button>
+                    </div>}
                 </div>
-            </Modal>
-        </main>
+                <Meta
+                    avatar={<Avatar src={post?.author?.avatar}/>}
+                    title={post?.author?.email}
+                />
+                <Divider/>
+                <Timeline>
+                    <Timeline.Item>{formatDateTime(post?.created_at)}</Timeline.Item>
+                    <Timeline.Item color="green">Отредактировано: {formatDateTime(post?.updated_at)}</Timeline.Item>
+                </Timeline>
+                <Divider/>
+                {post?.text}
+                <Divider/>
+                <div>
+                    {post?.tags?.map(tag => <Tag key={tag}>{tag}</Tag>)}
+                </div>
+
+                <Modal
+                    title="Подтверждение"
+                    visible={isConfirmationDialogVisible}
+                    onOk={onDeleteConfirmedClick}
+                    okText='Да, удалить'
+                    onCancel={onDeleteCanceledClick}
+                    cancelText='Отмена'
+                    centered={true}>
+                    <div className={css.questionIconAndText}>
+                        <QuestionCircleOutlined className={css.questionIcon}/>
+                        <span>Вы уверены, что хотите удалить данный постинг?</span>
+                    </div>
+                </Modal>
+            </main>
+        </Spinner>
     )
 }
 
--- src/page/PostPageView.jsx
+++ src/page/PostPageView.jsx
@@ -1,46 +1,17 @@
-import React, { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
+import React from 'react';
 
 import css from './page.module.css';
-
-import { message } from 'antd';
 
 import PostPageHeader from '../component/Header/PostPageHeader';
 import PostPageFooter from '../component/Footer/PostPageFooter';
 import PostContentView from '../component/Content/PostContentView';
-import { getPostById } from '../service/post-service';
-import Spinner from '../component/Spinner/Spinner';
 
 function PostPageView() {
-
-    const {postId} = useParams();
-    const [post, setPost] = useState({});
-    const [isPostLoadingInProgress, setIsPostLoadingInProgress] = useState(false);
-
-    useEffect(() => {
-        if (!!postId) {
-            setIsPostLoadingInProgress(true);
-
-            getPostById(postId)
-                .then(post => {
-                    setPost(post);
-                    setIsPostLoadingInProgress(false);
-                })
-                .catch(error => {
-                    setPost({});
-                    setIsPostLoadingInProgress(false);
-
-                    message.error(`Не удалось загрузить постинг для просмотра: ${error.message}`);
-                });
-        }
-    }, [postId]);
 
     return (
         <div className={css.commonPage}>
             <PostPageHeader/>
-            <Spinner spinning={isPostLoadingInProgress}>
-                <PostContentView post={post}/>
-            </Spinner>
+            <PostContentView/>
             <PostPageFooter/>
         </div>
     )
